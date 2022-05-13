0404c7b8ca0387054bf5ceaeb29afe2e7ea40c70
HUDWARE <udoughong@hotmail.com>
2022-05-13 17:31:32 +0800

    update n 20220513 1731

---
+++ Solution4OpenGauss/ConsoleApp4OpenGauss/Program.cs
@@ -0,0 +1,221 @@
+﻿using Npgsql;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4OpenGauss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+namespace Driver
+{
+    public class AzurePostgresCreate
+    {
+        // Obtain connection string information from the portal
+        //
+        private static string Host = "localhost";
+        private static string User = "sa";
+        private static string DBname = "benchmark";
+        private static string Password = "Trq@7251";
+        private static string Port = "5432";
+
+        static void Main(string[] args)
+        {
+            // Build connection string using parameters from portal
+            //
+            string connString =
+                String.Format(
+                    "Server={0};Username={1};Database={2};Port={3};Password={4};SSLMode=Prefer",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+
+            using (var conn = new NpgsqlConnection(connString))
+
+            {
+                Console.Out.WriteLine("Opening connection");
+                conn.Open();
+
+                using (var command = new NpgsqlCommand("DROP TABLE IF EXISTS inventory", conn))
+                {
+                    command.ExecuteNonQuery();
+                    Console.Out.WriteLine("Finished dropping table (if existed)");
+
+                }
+
+                using (var command = new NpgsqlCommand("CREATE TABLE inventory(id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER)", conn))
+                {
+                    command.ExecuteNonQuery();
+                    Console.Out.WriteLine("Finished creating table");
+                }
+
+                using (var command = new NpgsqlCommand("INSERT INTO inventory (name, quantity) VALUES (@n1, @q1), (@n2, @q2), (@n3, @q3)", conn))
+                {
+                    command.Parameters.AddWithValue("n1", "banana");
+                    command.Parameters.AddWithValue("q1", 150);
+                    command.Parameters.AddWithValue("n2", "orange");
+                    command.Parameters.AddWithValue("q2", 154);
+                    command.Parameters.AddWithValue("n3", "apple");
+                    command.Parameters.AddWithValue("q3", 100);
+
+                    int nRows = command.ExecuteNonQuery();
+                    Console.Out.WriteLine(String.Format("Number of rows inserted={0}", nRows));
+                }
+            }
+
+            Console.WriteLine("Press RETURN to exit");
+            Console.ReadLine();
+        }
+    }
+
+    public class AzurePostgresRead
+    {
+        // Obtain connection string information from the portal
+        //
+        private static string Host = "localhost";
+        private static string User = "sa";
+        private static string DBname = "benchmark";
+        private static string Password = "Trq@7251";
+        private static string Port = "5432";
+
+        static void Main(string[] args)
+        {
+            // Build connection string using parameters from portal
+            //
+            string connString =
+                String.Format(
+                    "Server={0}; User Id={1}; Database={2}; Port={3}; Password={4};SSLMode=Prefer",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+
+                Console.Out.WriteLine("Opening connection");
+                conn.Open();
+
+
+                using (var command = new NpgsqlCommand("SELECT * FROM inventory", conn))
+                {
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(
+                            string.Format(
+                                "Reading from table=({0}, {1}, {2})",
+                                reader.GetInt32(0).ToString(),
+                                reader.GetString(1),
+                                reader.GetInt32(2).ToString()
+                                )
+                            );
+                    }
+                    reader.Close();
+                }
+            }
+
+            Console.WriteLine("Press RETURN to exit");
+            Console.ReadLine();
+        }
+    }
+
+    public class AzurePostgresUpdate
+    {
+        // Obtain connection string information from the portal
+        //
+        private static string Host = "localhost";
+        private static string User = "sa";
+        private static string DBname = "benchmark";
+        private static string Password = "Trq@7251";
+        private static string Port = "5432";
+
+        static void Main(string[] args)
+        {
+            // Build connection string using parameters from portal
+            //
+            string connString =
+                String.Format(
+                    "Server={0}; User Id={1}; Database={2}; Port={3}; Password={4};SSLMode=Prefer",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+
+                Console.Out.WriteLine("Opening connection");
+                conn.Open();
+
+                using (var command = new NpgsqlCommand("UPDATE inventory SET quantity = @q WHERE name = @n", conn))
+                {
+                    command.Parameters.AddWithValue("n", "banana");
+                    command.Parameters.AddWithValue("q", 200);
+                    int nRows = command.ExecuteNonQuery();
+                    Console.Out.WriteLine(String.Format("Number of rows updated={0}", nRows));
+                }
+            }
+
+            Console.WriteLine("Press RETURN to exit");
+            Console.ReadLine();
+        }
+    }
+
+    public class AzurePostgresDelete
+    {
+        // Obtain connection string information from the portal
+        //
+        private static string Host = "localhost";
+        private static string User = "sa";
+        private static string DBname = "benchmark";
+        private static string Password = "Trq@7251";
+        private static string Port = "5432";
+
+        static void Main(string[] args)
+        {
+            // Build connection string using parameters from portal
+            //
+            string connString =
+                String.Format(
+                    "Server={0}; User Id={1}; Database={2}; Port={3}; Password={4};SSLMode=Prefer",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                Console.Out.WriteLine("Opening connection");
+                conn.Open();
+
+                using (var command = new NpgsqlCommand("DELETE FROM inventory WHERE name = @n", conn))
+                {
+                    command.Parameters.AddWithValue("n", "orange");
+
+                    int nRows = command.ExecuteNonQuery();
+                    Console.Out.WriteLine(String.Format("Number of rows deleted={0}", nRows));
+                }
+            }
+
+            Console.WriteLine("Press RETURN to exit");
+            Console.ReadLine();
+        }
+    }
+}
